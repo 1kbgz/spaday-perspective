a090002acc2d3b9217608578a6bc22c2a2c16dd3
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-09-09 20:04:26 -0400

    Bump version: 0.5.0 → 0.5.1
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- spaday_perspective/__init__.py
+++ spaday_perspective/__init__.py
@@ -4,7 +4,7 @@ from spaday import ComponentPackage
 
 from .components import PerspectivePanel
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 package = ComponentPackage(
     name="perspective",
